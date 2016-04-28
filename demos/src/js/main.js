d7b5db560afefeaf412f50eae7c3197af70de1bf
St. John Peaster <stjohn3d@outlook.com>
2016-04-28 18:11:07 -0400

    WIP - A very broken commit
    Trying to bring the timber-ui npm module into the demos project. It looks like jsx isn't being transpiled.
    
    Tried using babel-register which has worked perfectly in the passed but isn't working right now for some reason.
    
    Also converted timber.js to 4 space tabs
    
    Tomorrow I'll look at how react-redux does it and try to follow that pattern

--- demos/src/js/main.js
+++ demos/src/js/main.js
@@ -1,4 +1,3 @@
-import '../styles/bootstrap.min.css'
 import '../styles/styles.scss'
 
 import React from 'react'
@@ -6,51 +5,53 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { compose, createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
-import TimberApp from './components/timber/TimberApp'
-import Container from './components/timber/Container'
-import Panel from './components/timber/Panel'
+// import TimberApp from './components/timber/TimberApp'
+// import Container from './components/timber/Container'
+// import Panel from './components/timber/Panel'
 import ToolExample1 from './components/tools/ToolExample1'
 import ToolExample2 from './components/tools/ToolExample2'
+import { TimberApp, Container, Panel } from 'timber-ui'
+console.log(TimberApp, Container, Panel)
 
 const createFinalStoreWithMiddleware = compose(
-	applyMiddleware(),
-	window.devToolsExtension ? window.devToolsExtension() : f => f
+    applyMiddleware(),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
 )(createStore)
 
 const store = createFinalStoreWithMiddleware(rootReducer);
 
 ReactDOM.render(
-	<Provider store={store}>
-		<TimberApp tools={[<ToolExample1 name="Blocks"/>, <ToolExample2/>]}>
-			<Container flow="VERTICAL">
-				<Panel>
-					<Container flow="HORIZONTAL">
-						<Panel width='300px' toolIndex="1">
-						</Panel>
-						<Panel>
-							<Container flow="VERTICAL">
-								<Panel height="10%">
-								</Panel>
-								<Panel>
-									<Container flow="HORIZONTAL">
-										<Panel>
-										</Panel>
-										<Panel>
-										</Panel>
-										<Panel width='250px'>
-										</Panel>
-									</Container>
-								</Panel>
-								<Panel height='10%'>
-								</Panel>
-							</Container>
-						</Panel>
-					</Container>
-				</Panel>
-				<Panel height='10%'>
-				</Panel>
-			</Container>
-		</TimberApp>
-	</Provider>,
-	document.getElementById('app')
+    <Provider store={store}>
+        <TimberApp tools={[<ToolExample1 name="Blocks"/>, <ToolExample2/>]}>
+            <Container flow="VERTICAL">
+                <Panel>
+                    <Container flow="HORIZONTAL">
+                        <Panel width='300px' toolIndex="1">
+                        </Panel>
+                        <Panel>
+                            <Container flow="VERTICAL">
+                                <Panel height="10%">
+                                </Panel>
+                                <Panel>
+                                    <Container flow="HORIZONTAL">
+                                        <Panel>
+                                        </Panel>
+                                        <Panel>
+                                        </Panel>
+                                        <Panel width='250px'>
+                                        </Panel>
+                                    </Container>
+                                </Panel>
+                                <Panel height='10%'>
+                                </Panel>
+                            </Container>
+                        </Panel>
+                    </Container>
+                </Panel>
+                <Panel height='10%'>
+                </Panel>
+            </Container>
+        </TimberApp>
+    </Provider>,
+    document.getElementById('app')
 );
--- timber-ui/timber.js
+++ timber-ui/timber.js
@@ -1,86 +1,83 @@
-'use strict';
-var jss = require('js-stylesheet');
-var TimberApp = require('./lib/timber-app');
-var Container = require('./lib/container');
-var Panel = require('./lib/panel');
-var MouseStore = require('./lib/stores/mouse-store');
-var UIStore = require('./lib/stores/ui-store');
+const jss = require('js-stylesheet')
+require('babel-register')({
+    // This will override `node_modules` ignoring - you can alternatively pass
+    // an array of strings to be explicitly matched or a regex / glob
+    ignore: false
+});
 
 module.exports = {
-	timberApp : TimberApp,
-	container : Container,
-	panel     : Panel,
-	mouseStore: MouseStore,
-	uiStore   : UIStore,
-	injectCSS : function() {
-		jss({
-			'body': {
-				'width'  : '100vw',
-				'height' : '100vh',
-				'padding': '0px',
-				'margin' : '0px'
-			},
-			'.timber-ui': {
-				'width' : '100%',
-				'height': '100%'
-			},
-			'.timber-container': {
-				'width'     : '100%',
-				'height'    : '100%',
-				'min-height': '200px',
-				'display'   : 'table'
-			},
-			'.timber-row': {
-				'display': 'table-row'
-			},
-			'.timber-panel': {
-				'display'         : 'table-cell',
-				'background-color': 'grey',
-				'border'          : '0.5px solid black',
-				'position'        : 'relative'
-			},
-			'@media (max-width: 420px)': {
-				'.timber-container, .timber-row, .timber-panel': {
-					'display'   : 'block',
-					'width'     : '100%',
-					'height'    : 'auto',
-					'min-height': 'auto'
-				}
-			},
-			'.timber-resize-h': {
-				'position'        : 'absolute',
-				'top'             : '0px',
-				'right'           : '-5px',
-				'width'           : '10px',
-				'height'          : '100%',
-				'z-index'         : '99999',
-				'background-color': 'transparent'
-			},
-			'.timber-resize-h:hover': {
-				'cursor': 'ew-resize'
-			},
-			'.timber-resize-v': {
-				'position'        : 'absolute',
-				'bottom'          : '-5px',
-				'left'            : '0px',
-				'width'           : '100%',
-				'height'          : '10px',
-				'z-index'         : '99999',
-				'background-color': 'transparent'
-			},
-			'.timber-resize-v:hover': {
-				'cursor': 'ns-resize'
-			},
-			'.timber-corner': {
-				'width'      : '0',
-				'height'     : '0',
-				'border-top' : '20px solid red',
-				'border-left': '20px solid transparent',
-				'float'      : 'right'
-			},
-			'.timber-corner:hover': {
-				'border-top': '20px solid white'
-			}
-		})
-	}
-};
+    TimberApp: {}, // require('./components/TimberApp.jsx'),
+    Container: {}, //require('./components/Container'),
+    Panel    : {}, //require('./components/Panel'),
+    injectCSS: () => {
+        jss({
+            'body': {
+                'width'  : '100vw',
+                'height' : '100vh',
+                'padding': '0px',
+                'margin' : '0px'
+            },
+            '.timber-ui': {
+                'width' : '100%',
+                'height': '100%'
+            },
+            '.timber-container': {
+                'width'     : '100%',
+                'height'    : '100%',
+                'min-height': '200px',
+                'display'   : 'table'
+            },
+            '.timber-row': {
+                'display': 'table-row'
+            },
+            '.timber-panel': {
+                'display'         : 'table-cell',
+                'background-color': 'grey',
+                'border'          : '0.5px solid black',
+                'position'        : 'relative'
+            },
+            '@media (max-width: 420px)': {
+                '.timber-container, .timber-row, .timber-panel': {
+                    'display'   : 'block',
+                    'width'     : '100%',
+                    'height'    : 'auto',
+                    'min-height': 'auto'
+                }
+            },
+            '.timber-resize-h': {
+                'position'        : 'absolute',
+                'top'             : '0px',
+                'right'           : '-5px',
+                'width'           : '10px',
+                'height'          : '100%',
+                'z-index'         : '99999',
+                'background-color': 'transparent'
+            },
+            '.timber-resize-h:hover': {
+                'cursor': 'ew-resize'
+            },
+            '.timber-resize-v': {
+                'position'        : 'absolute',
+                'bottom'          : '-5px',
+                'left'            : '0px',
+                'width'           : '100%',
+                'height'          : '10px',
+                'z-index'         : '99999',
+                'background-color': 'transparent'
+            },
+            '.timber-resize-v:hover': {
+                'cursor': 'ns-resize'
+            },
+            '.timber-corner': {
+                'width'      : '0',
+                'height'     : '0',
+                'border-top' : '20px solid red',
+                'border-left': '20px solid transparent',
+                'float'      : 'right'
+            },
+            '.timber-corner:hover': {
+                'border-top': '20px solid white'
+            }
+        })
+    }
+}
