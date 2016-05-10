1bc3bd73ed4a438743b88eb253a95c8141d38f31
St. John Peaster <stjohn3d@outlook.com>
2016-05-10 07:59:53 -0400

    Split Vertical - works but maybe we could do it with less code...

--- module/src/reducers/split-panel-reducer.js
+++ module/src/reducers/split-panel-reducer.js
@@ -56,12 +56,65 @@ const splitHorizontal = (state, payload) => {
     }
 }
 
+const splitVertical = (state, payload) => {
+    const { panelID, parentContainerFlow, resizeID, parentID, parentIndex, toolIndex } = payload
+    const { children } = layout(state.index).getProps(parentID)
+
+
+    switch ( parentContainerFlow ) {
+        case flow.HORIZONTAL:
+            const containerID = generateID()
+            const rightPanelID = generateID()
+            return Object.assign({}, state, {
+                index: {
+                    ...state.index,
+                    [containerID]: {
+                        type: 'Container',
+                        parentID: panelID,
+                        parentIndex: 0,
+                        flow: flow.VERTICAL
+                    },
+                    [resizeID]: {
+                        type: 'Panel',
+                        parentID: containerID,
+                        parentIndex: 0,
+                        toolIndex,
+                    },
+                    [rightPanelID]: {
+                        type: 'Panel',
+                        parentID: containerID,
+                        parentIndex: 1,
+                        toolIndex,
+                    }
+                }
+            })
+        case flow.VERTICAL:
+            let newIndex = Object.assign({}, state.index, {
+                [resizeID]: {
+                    type: 'Panel',
+                    parentID,
+                    parentIndex: parentIndex,
+                    toolIndex,
+                }
+            })
+            children.forEach((child) => {
+                newIndex[child.id].parentIndex = child.parentIndex < parentIndex ?
+                    child.parentIndex : child.parentIndex + 1
+            })
+            return Object.assign({}, state, {
+                index: newIndex
+            })
+        default:
+            return state
+    }
+}
+
 export default (state, payload) => {
     switch ( payload.intent ) {
         case UI.SPLIT.HORIZONTAL:
             return splitHorizontal( state, payload )
         case UI.SPLIT.VERTICAL:
-            break
+            return splitVertical( state, payload )
         default:
             return state
     }
